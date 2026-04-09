45697942b35bef9983e3da20aa0c2b4c4fa24ccf
Benjamin Nwoye <121277911+Donbenny1@users.noreply.github.com>
2026-04-09 19:02:16 +0100

    updates made

--- config.ts
+++ config.ts
@@ -5,3 +5,9 @@ export const trading = {
 	botTimer: 360 // In mins 5, 20, 10
 };
 
+export const timestamp_str_alt = (date: string | number) => {
+	const d = new Date(date);
+	const date_ = d.getFullYear() + "-" + ((d.getUTCMonth() + 1) < 10 ? "0" + (d.getUTCMonth() + 1) : (d.getUTCMonth() + 1)) + "-" + (d.getDate() < 10 ? "0" + d.getDate() : d.getDate());
+	const time_ = (d.getHours() < 10 ? "0" + d.getHours() : d.getHours()) + ":" + (d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()) + ":" + (d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds());
+	return date_ + " " + time_;
+};
